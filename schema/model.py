ce659ef853bf4577a758a153116fe75b6f4f2955
Claudius Kienle <claudius.kienle@artiminds.com>
2024-11-08 08:47:36 +0100

    started working on config settings

--- schema/model.py
+++ schema/model.py
@@ -306,6 +306,11 @@ class PyPIPathRequirement(_PyPIRequirement):
     editable: bool | None = Field(
         None, description="If `true` the package will be installed as editable"
     )
+    config_settings: dict[NonEmptyStr, NonEmptyStr] | None = Field(
+        None,
+        description="Settings to pass to the PEP 517 build backend, specified as KEY=VALUE pairs",
+        examples=[{"editable_mode": "compat"}],
+    )
     subdirectory: NonEmptyStr | None = Field(
         None, description="The subdirectory in the repo, a path from the root of the repo."
     )
